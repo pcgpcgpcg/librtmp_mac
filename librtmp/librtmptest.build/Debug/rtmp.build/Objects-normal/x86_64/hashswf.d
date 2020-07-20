0e6287d731757b52af5081bedd806c9994d0134d
pcg <pcgpcg@outlook.com>
2020-07-20 22:58:35 +0800

    call ok

--- librtmp/librtmptest.build/Debug/rtmp.build/Objects-normal/x86_64/hashswf.d
+++
@@ -1,6 +0,0 @@
-dependencies: /Users/mac/pcgdev/cpp/librtmptest/librtmp/hashswf.c \
-  /Users/mac/pcgdev/cpp/librtmptest/librtmp/rtmp_sys.h \
-  /Users/mac/pcgdev/cpp/librtmptest/librtmp/rtmp.h \
-  /Users/mac/pcgdev/cpp/librtmptest/librtmp/amf.h \
-  /Users/mac/pcgdev/cpp/librtmptest/librtmp/log.h \
-  /Users/mac/pcgdev/cpp/librtmptest/librtmp/http.h
--- main.cpp
+++ main.cpp
@@ -11,9 +11,21 @@
 
 int main(int /*argc*/, char* /*argv*/[])
 {
-    RTMP* rtmp = RTMP_Alloc();
-    RTMP_Init(rtmp);
-    RTMPResult ret = RTMP_SetupURL(rtmp, (char*)"rtmp://49.233.136.247/live/pcg");
+    int result = rtmp_open_for_write("rtmp://49.233.136.247/live/pcg", 1280, 720);
+    if(result<=0){
+        printf("%s","open failed");
+        return 0;
+    }
+    bool bConnected = rtmp_is_connected();
+    if(!bConnected){
+        printf("%s","connect failed");
+        return 0;
+    }
+    //Write h264 nal units with this function
+    //rtmp_sender_write_video_frame(<#uint8_t *data#>, <#int size#>, <#uint64_t dts_us#>, <#int key#>, <#uint32_t abs_ts#>)
+    
+    rtmp_close();
+    
     
     printf("%s","hello world");
     
